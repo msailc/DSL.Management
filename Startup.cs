c150e31f4fe911e3ba3012fc2ca11314b303ece5
Mustafa <mustafaislamovich@gmail.com>
2023-03-22 00:56:02 +0100

    implemented oauth endpoint

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,10 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using PagebaTask.Controllers;
+using PagebaTask.Models;
+
 namespace PagebaTask
 {
     public class Startup
@@ -13,6 +20,33 @@ namespace PagebaTask
         {
             services.AddCors();
             services.AddControllers();
+                // Add DbContext to the DI container
+    services.AddDbContext<DataContext>(options =>
+        options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+    // Add the AuthController to the DI container
+    services.AddScoped<AuthController>();
+            services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "GitHub";
+})
+.AddCookie()
+.AddOAuth("GitHub", options =>
+{
+    options.ClientId = Configuration["GitHub:ClientId"];
+    options.ClientSecret = Configuration["GitHub:ClientSecret"];
+    options.CallbackPath = "/auth/callback";
+
+    options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+    options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+    options.UserInformationEndpoint = "https://api.github.com/user";
+
+    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "login");
+    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+    options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email", ClaimValueTypes.Email);
+});
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -23,6 +57,8 @@ namespace PagebaTask
             }
 
             app.UseAuthentication();
+            app.UseAuthorization();
+
 
             app.UseRouting();
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
