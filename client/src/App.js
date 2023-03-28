901fe40c7f5d1e0c1d78dc432ce4bbcc6f43c24a
damirtodic <damir.todic@stu.ibu.edu.ba>
2023-03-29 01:53:01 +0200

    Added Log In page and Home Page using routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+import LogIn from './LogIn';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LogIn />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
