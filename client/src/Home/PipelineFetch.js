65b23321c8c392ae3d8ab50918249ccb4df0781e
Mustafa <mustafaislamovich@gmail.com>
2023-05-24 19:24:15 +0200

    made list of pipelines expandable for every pipeline

--- client/src/Home/PipelineFetch.js
+++ client/src/Home/PipelineFetch.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./PipelineFetch.css"; // Import the CSS file
 
 export default function PipelineFetch() {
     const [pipelines, getPipelines] = useState([]);
@@ -9,21 +10,37 @@ export default function PipelineFetch() {
             .get(`http://localhost:5017/pipeline`)
             .then((res) => {
                 console.log(res);
-                getPipelines(Array.isArray(res.data.$values) ? res.data.$values : []);
+                getPipelines(
+                    Array.isArray(res.data.$values)
+                        ? res.data.$values.map((pipeline) => ({ ...pipeline, collapsed: true }))
+                        : []
+                );
             })
             .catch((err) => console.log(err));
     }, []);
 
+    const togglePipeline = (id) => {
+        getPipelines((prevPipelines) =>
+            prevPipelines.map((pipeline) =>
+                pipeline.id === id ? { ...pipeline, collapsed: !pipeline.collapsed } : { ...pipeline, collapsed: true }
+            )
+        );
+    };
+
     return (
-        <div>
+        <div className="pipeline-container">
             {pipelines.map((pipeline) => (
-                <div key={pipeline.id}>
-                    <ul>
-                        <li>{pipeline.name}</li>
-                    </ul>
+                <div key={pipeline.id} className="pipeline">
+                    <button onClick={() => togglePipeline(pipeline.id)}>{pipeline.name}</button>
+                    {!pipeline.collapsed && (
+                        <ul>
+                            <li>{pipeline.name}</li>
+                            <li>Sample text</li>
+                            <li>More sample text</li>
+                        </ul>
+                    )}
                 </div>
             ))}
         </div>
     );
 }
-
